92d3d1565a7c06e455525545548a1fd22aec1f3c
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-07-27 15:57:27 +0200

    [UPD] bonus 2

--- snacks.js
+++ snacks.js
@@ -12,7 +12,7 @@ async function getChefBirthday(id) {
     catch (error) {
         throw new Error(`C'è stato un problema nel recuperare la ricetta: ${error}`)
     }
-    const userId = await recipe.userId;
+    const userId = recipe.userId;
     const chefInfo = await fetchObj(`https://dummyjson.com/users/${userId}`)
     const ChefDate = chefInfo.birthDate
     return ChefDate
@@ -20,10 +20,12 @@ async function getChefBirthday(id) {
 
 (async () => {
     try {
-        const ChefDate = await getChefBirthday(1)
-        console.log(`La data di nascita è : ${ChefDate}`)
+        const ChefDate = await getChefBirthday(2)
+        const formattedDate = dayjs(ChefDate).format("DD/MM/YYYY")
+        console.log(`La data di nascita è : ${formattedDate}`)
     }
     catch (error) {
         console.error(error)
     }
+
 })();
